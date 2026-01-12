e83762a7f512321143e2ce538cc4b1626732ead2
an_yan_yang <112800472+yanggogi1423@users.noreply.github.com>
2026-01-13 07:37:31 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    
+    int a, b;
+    cin >> a >> b;
+    
+    double c = a/(double)b;
+    
+    cout.precision(10);
+    cout << c;
+    
+    return 0;
+}
+ 
+ 
+ 
